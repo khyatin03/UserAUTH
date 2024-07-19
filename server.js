714a45700fd51a4ddf0e3c3aa2450d0aebf92f40
khyatin03 <111226405+khyatin03@users.noreply.github.com>
2024-07-19 12:33:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://khyatin2003:Ganesh%402013@cluster0.zhanlfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
